a0a411f016be83234715b16c110f1af79ca98e91
LaymBoro <79944341+LaymBoro@users.noreply.github.com>
2024-12-02 23:18:38 +0300

    models

--- fitness_back/models/DailyRation.go
+++ fitness_back/models/DailyRation.go
@@ -5,6 +5,6 @@ import "time"
 type DailyRation struct {
 	DailyRationID uint      `gorm:"primary_key;auto_increment"`
 	UserID        uint      `gorm:"not null"`
-	Ration        []Dish    `gorm:"not null"`
+	Ration        string    `gorm:"not null"`
 	CreatedAt     time.Time `gorm:"autoCreateTime"`
 }
--- fitness_back/models/Dish.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type Dish struct {
-	DishID   uint   `gorm:"primary_key;auto_increment"`
-	Name     string `gorm:"not null"`
-	Calories string `gorm:"not null"`
-}
